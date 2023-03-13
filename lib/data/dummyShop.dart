659f772380b1a6e8d2a620567d771f4d50a91079
cartoon <defoko12@gmail.com>
2023-03-13 16:43:51 +0700

    test

---
+++ lib/data/dummyShop.dart
@@ -0,0 +1,51 @@
+class Data {
+  List allstores = [
+    {
+      "name": "ร้านข้าวมันไก่",
+      "image":
+          "https://www.thaismescenter.com/wp-content/uploads/2018/09/c1.jpg",
+      "lat": 13.043618,
+      "lng": 80.279824,
+      "distance": 0,
+    },
+    {
+      "name": "ร้านขายผัก",
+      "image":
+          "https://img.wongnai.com/p/1920x0/2021/08/05/7b572a233d7d42639f0f82d7ca8ad4ec.jpg",
+      "lng": 80.265799,
+      "distance": 0,
+    },
+    {
+      "name": "ร้านขายผลไม้",
+      "image":
+          "https://sls-prod.api-onscene.com/partner_files/trueidintrend/9214/IMG_8193.jpg",
+      "lat": 13.092798,
+      "lng": 80.269044,
+      "distance": 0,
+    },
+    {
+      "name": "ร้านขายเนื้อ",
+      "image":
+          "https://media.komchadluek.net/uploads/images/contents/w1024/2022/01/cNJFWHdOWEcw08JeR0Jc.webp",
+      "lat": 13.086068,
+      "lng": 80.270278,
+      "distance": 0,
+    },
+    {
+      "name": "ร้านขายซูชิ",
+      "image":
+          "https://www.thaifranchisecenter.com/document/franchise/picture/document_7141_p5_20210902171010.jpg",
+      "lat": 13.083790,
+      "lng": 80.278958,
+      "distance": 0,
+    },
+    {
+      "name": "ร้านขายปลา",
+      "image":
+          "https://i0.wp.com/wekorat.com/wp-content/uploads/2014/08/IMG_2415.jpg",
+      "lat": 13.086265,
+      "lng": 80.291128,
+      "distance": 0,
+    }
+  ];
+}
